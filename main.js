3ec518ab55f8cc4e5469b728731f10944f3b0b2b
Muhammad Azhan <156644698+Azhandev@users.noreply.github.com>
2024-05-04 23:23:08 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const display = document.getElementById("display");
 let timer = null;
 let startTime = 0;
 let elapsedTime = 0
-let isRunning = fals
+let isRunning = fal
 
 function start() 
     if (!isRunning) {
